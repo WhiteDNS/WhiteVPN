0d1859f82a1f9013d9c17f67e1e6db194704cb55
Pedi <pedram.marandi@gmail.com>
2026-08-20 12:11:49 +1000

    update version to v1.6.0 (#14)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,7 +80,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 78
-        versionName = "1.5.0"
+        versionName = "1.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
