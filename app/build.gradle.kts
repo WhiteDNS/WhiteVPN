2061d3398464238f50c84947c9f2426a5d04e70b
iampedii <7813872+iampedii@users.noreply.github.com>
2026-08-21 12:55:10 +1000

    version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,7 +80,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 79
-        versionName = "1.6.0"
+        versionName = "1.6.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
